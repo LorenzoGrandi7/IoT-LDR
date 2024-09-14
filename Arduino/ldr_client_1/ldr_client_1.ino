a241486e87379289513a97215c8806177999eb24
Lorenzo <lorenzo.grandi7@studio.unibo.it>
2024-09-14 12:51:03 +0200

    Update

--- Arduino/ldr_client_1/ldr_client_1.ino
+++ Arduino/ldr_client_1/ldr_client_1.ino
@@ -4,8 +4,8 @@
 #include "PubSubClient.h"
 
 // Wi-Fi settings ========================
-// #define WIFI_SSID *************************
-// #define WIFI_PW   *************************
+// #define WIFI_SSID FRITZ!Box 7590 Fabio
+// #define WIFI_PW   73622386758528572341
 // #define COAP_SERVER IPAddress(x, x, x, x)     // Using home Wi-Fi
 // #define MQTT_SERVER "x.x.x.x"
 
